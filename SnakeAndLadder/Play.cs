dd48bcd484516a77f7a2ada16f654cc8db6604fb
mahesh <lokilokesh1680@gmail.com>
2023-03-13 22:13:13 +0530

    [lokesh]add:folder is added

--- SnakeAndLadder/Play.cs
+++ SnakeAndLadder/Play.cs
@@ -10,7 +10,16 @@ namespace SnakeAndLadder
     {
         int NO_PLAY, Ladder = 1;
         int Playerposition = 0, count = 0;
+        Random random = new Random();
+        public int DieRoll()
+        {
+            int die = random.Next(1, 7);
+            Console.WriteLine("die value----->  " + die);
+            count++;
+            return die;
+        }
     }
 }
+
 
 
