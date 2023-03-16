e4f9b3e55e18fc1cdea414dde05ce63cd9bd7b19
mahesh <lokilokesh1680@gmail.com>
2023-03-16 16:51:53 +0530

    [lokesh]add:folder is added

--- SnakeAndLadder/Play.cs
+++ SnakeAndLadder/Play.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,7 @@ namespace SnakeAndLadder
 {
     public class Play
     {
-        int NO_PLAY, Ladder = 1;
+        int NO_PLAY, LADDER = 1, SNAKE = 2, WINNING_POSITION = 100;
         int Playerposition = 0, count = 0;
         Random random = new Random();
         public int DieRoll()
@@ -18,8 +19,32 @@ namespace SnakeAndLadder
             count++;
             return die;
         }
+        public void Game()
+        {
+            int position = 0;
+            Play p=new Play();
+            p.DieRoll();
+            Random random=new Random();
+            while (this.Playerposition < WINNING_POSITION)
+            {
+                int option = random.Next(0, 3);
+                switch (option)
+                {
+                    case NO_PLAY:
+                        break;
+                    case LADDER:
+                        this.Playerposition += DieRoll();
+                        break;
+                    case SNAKE:
+                        this.Playerposition -= DieRoll();
+                        if (this.Playerposition < 0)
+                            this.Playerposition = 0;
+                        break;
+                }
+            }
+            Console.WriteLine("Player position------>"+Playerposition);
+        }
     }
 }
-
 
 
